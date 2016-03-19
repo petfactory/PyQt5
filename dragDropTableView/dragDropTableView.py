6c778781db76824b4b08d660a10c90aaea4e219d
Johan Borgström <johan@petfactory.se>
2016-03-19 14:16:42 +0100

    add icon, delete rows, read csv

--- dragDropTableView/dragDropTableView.py
+++ dragDropTableView/dragDropTableView.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import sys
-from PyQt5.QtWidgets import QWidget, QDesktopWidget, QApplication, QMainWindow, QFrame, QTableView, QVBoxLayout, QAbstractItemView
+import sys, os
+from PyQt5.QtWidgets import QWidget, QDesktopWidget, QApplication, QMainWindow, QFrame, QTableView, QVBoxLayout, QAbstractItemView, QFileDialog, QAction
 from PyQt5 import QtGui, QtCore
-
+import csv
+import shutil
 
 class MyTableView(QTableView):
+
+    def __init__(self, main_win=None, parent=None):
+        super(MyTableView, self).__init__(parent)
+        self.main_win = main_win
 
     def dragMoveEvent(self, event):
         index = self.indexAt(event.pos())
@@ -42,14 +47,15 @@ class MyTableView(QTableView):
 
                     self.clearSelection()
 
-                    #new_name = '{}_{}'.format(self.item(row, 0).text(), self.item(row, 1).text())
-                    #copy_to_folder(links[0], self.root_dir, '{}.pdf'.format(new_name))
+                    new_name = '{} {}'.format(self.model().item(row, 0).text(), self.model().item(row, 1).text())
+                    self.main_win.copy_to_folder(links[0],'{}.pdf'.format(new_name))
+                    #print(self.main_win)
 
 
 class MyModel(QtGui.QStandardItemModel):
 
     def supportedDragActions(self):
-        return Qt.LinkAction | Qt.CopyAction
+        return QtCore.Qt.LinkAction | QtCore.Qt.CopyAction
 
     def canDropMimeData(self, *args):
         return True
@@ -59,56 +65,149 @@ class Example(QMainWindow):
     def __init__(self):
         super().__init__()
         
+        self.root_dir = '/Users/johan/Desktop/apa'
         #self.resize(300, 400)
         #self.center()
         self.setGeometry(200, 200, 300, 400)
         
-        
+        menubar = self.menuBar()
+        menubar.setNativeMenuBar(False)
+        file_menu = menubar.addMenu('&File')
+
+        open_action = QAction(QtGui.QIcon(self.resource_path('open.png')), '&Open', self)
+        open_action.setShortcut('Ctrl+O')
+        open_action.setStatusTip('Open File')
+        open_action.triggered.connect(self.read_csv)
+        file_menu.addAction(open_action)
+
+        delete_action = QAction('Delete', self)
+        delete_action.triggered.connect(self.delete_selected_rows)
+        file_menu.addAction(delete_action)
+
+
         main_frame = QFrame()
         self.setCentralWidget(main_frame)
         vbox = QVBoxLayout(main_frame)
 
         #self.tableview = QTableView()
-        self.tableview = MyTableView()
+        self.tableview = MyTableView(main_win=self)
+
         self.tableview.setDropIndicatorShown(True)
         self.tableview.setDragEnabled(True)
         self.tableview.setAcceptDrops(True)
         self.tableview.setDragDropMode(QTableView.DragDrop)
         self.tableview.setDefaultDropAction(QtCore.Qt.LinkAction)
         self.tableview.setDropIndicatorShown(True)
-
-
         vbox.addWidget(self.tableview)
 
-        #self.model = QtGui.QStandardItemModel()
         self.model = MyModel()
         self.tableview.setModel(self.model)
 
-        for i in range(10):
-        
-            item = QtGui.QStandardItem('Item {}'.format(i))
-            item.setFlags(QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsDragEnabled | QtCore.Qt.ItemIsDropEnabled)
-            self.model.setItem(i,0,item)
+        self.setWindowTitle('Center')
 
-            item = QtGui.QStandardItem('Some info {}'.format(i))
-            item.setFlags(QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsDragEnabled | QtCore.Qt.ItemIsDropEnabled)
-            self.model.setItem(i,1,item)
+        self.read_csv()
 
-
-        self.setWindowTitle('Center')    
         self.show()
-        
-        
+
+    def delete_selected_rows(self):
+
+        selected_indexes =  self.tableview.selectionModel().selectedRows()
+ 
+        sel_rows = []
+        for index in selected_indexes:
+            sel_rows.append(index.row())
+
+        sel_rows.sort()
+        num_rows = len(sel_rows)
+
+        for i in range(num_rows):
+            self.model.removeRow(sel_rows[num_rows-i-1])
+
+
+    def populate_model(self, header_labels, data_list):
+
+        self.model.clear()
+        # set the first 3 headers, by slicing the off
+        self.model.setHorizontalHeaderLabels(header_labels[:3])
+
+        for row, data in enumerate(data_list):
+
+            date_item = QtGui.QStandardItem(data[0])
+            date_item.setFlags(QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsDragEnabled | QtCore.Qt.ItemIsDropEnabled)
+            self.model.setItem(row,0,date_item)
+
+            desc_item = QtGui.QStandardItem(data[1])
+            desc_item.setFlags(QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsDragEnabled | QtCore.Qt.ItemIsDropEnabled)
+            self.model.setItem(row,1,desc_item)
+
+            amount_item = QtGui.QStandardItem(data[2])
+            amount_item.setFlags(QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsDragEnabled | QtCore.Qt.ItemIsDropEnabled)
+            self.model.setItem(row,2,amount_item)
+
+        self.tableview.horizontalHeader().setStretchLastSection(True)
+
+    def resource_path(self, relative_path):
+        """ Get absolute path to resource, works for dev and for PyInstaller """
+        try:
+            # PyInstaller creates a temp folder and stores path in _MEIPASS
+            base_path = sys._MEIPASS
+        except Exception:
+            base_path = os.path.abspath(".")
+
+        return os.path.join(base_path, relative_path)
+
+
     def center(self):
         
         qr = self.frameGeometry()
         cp = QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
-        
-        
+    
+
+    def copy_to_folder(self, source_file, new_name):
+
+        dest_file = os.path.join(self.root_dir, new_name)
+
+        if os.path.isfile(dest_file):
+            print('File exists. Overwrite?')
+
+        shutil.copy(source_file, dest_file)
+
+    def read_csv(self):
+
+        #file_name, selected_filter = QFileDialog.getOpenFileName(None, 'Load csv file', None, filter='CSV (*.csv)')
+        file_name = '/Users/johan/Desktop/test.csv'
+
+        if file_name:
+
+            data_list = []
+            header_list = None
+            with open(file_name) as csvfile:    
+                dialect = csv.Sniffer().sniff(csvfile.read(1024))
+                csvfile.seek(0)
+                reader = csv.reader(csvfile, dialect)
+
+                for row_index, row in enumerate(reader):
+
+                    if row_index == 0:
+                        print(len(row))
+                        header_list = row
+                    else:
+                        print(row)
+                        data_list.append(row)
+
+            self.populate_model(header_list, data_list)
+
+
+
+
 if __name__ == '__main__':
     
+
     app = QApplication(sys.argv)
     ex = Example()
-    sys.exit(app.exec_()) 
+    sys.exit(app.exec_())
+    
+
+
